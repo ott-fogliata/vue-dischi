07ff802ab73916e0450a9d5ef973779f78c4a8c0
santocaridi99 <santocaridi99@gmail.com>
2022-02-02 10:39:15 +0100

    axios API

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="app">
+    {{disks}}
   </div>
 </template>
 
 <script>
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      disks: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((res) => {
+        this.disks = res.data.response;
+      });
+  },
+};
 </script>
 
 <style lang="scss">
-
 </style>
